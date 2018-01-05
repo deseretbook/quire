95b73cc0562e6aecdb6ffb66aa7418d8949cacba
Matthew Nielsen <xunker@pyxidis.org>
2018-01-05 13:56:44 -0700

    Use instance variant in recursion instead of reference passing, there was some kind of recursion problem with it. Bump gem version.

--- lib/quire/toc.rb
+++ lib/quire/toc.rb
@@ -6,30 +6,30 @@ class Quire::Toc
   # returns navMap->navPoints->content_src as array order as in the toc, with
   # anchors ("xxx.html#xyz") removed if they are present. Unique and sorted.
   def nav_point_content_sources
-    found_content_sources = []
-    find_content_sources(nav_map, found_content_sources)
-    found_content_sources.map{|s| s.split('#').first}.compact.uniq
+    @found_content_sources = []
+    find_content_sources(nav_map)
+    @found_content_sources.map{|s| s.split('#').first}.compact.uniq
   end
 
 private
 
-  def find_content_sources(node, found = [])
+  def find_content_sources(node)
     if node.kind_of? Array
-      node.each { |n| find_content_sources(n, found) }
+      node.each { |n| find_content_sources(n) }
     elsif node.kind_of? Hash
       node.keys.each do |key|
         if key == 'content'
           if node[key].is_a? Array
-            found += node[key].map{|n| n['src']}
+            @found_content_sources += node[key].map{|n| n['src']}
           else
-            found << node[key]['src']
+            @found_content_sources << node[key]['src']
           end
         else
-          find_content_sources(node[key], found)
+          find_content_sources(node[key])
         end
       end
     end
-    found
+    @found_content_sources
   end
 
   def nav_map
